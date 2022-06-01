a9ace999816d47066ca6c1d102f96770726c50d0
ukflava <ukflava@gmail.com>
2022-06-01 09:51:41 -0700

    letterCountLowerCase

--- countLetters.js
+++ countLetters.js
@@ -4,10 +4,10 @@ const assertEqual = function(actual, expected) {
 };
 const countLetters = function(str){
   let answer = {};
-  for (let letter of str.replace(/\s+/g, "")) { answer[letter] ? answer[letter] +=1: answer[letter] =1}
+  for (let letter of str.replace(/\s+/g, "").toLowerCase()) { answer[letter] ? answer[letter] +=1: answer[letter] =1}
 
   return answer
 }
-// console.log(countLetters("lighthouse in the house"))
+console.log(countLetters("lighthouse in the house"))
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
