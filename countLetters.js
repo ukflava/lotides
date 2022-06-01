87eeb2cf04fc5425d581db3cefc2fde62397a68c
ukflava <ukflava@gmail.com>
2022-06-01 09:50:22 -0700

    letterCount

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected
+    ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+const countLetters = function(str){
+  let answer = {};
+  for (let letter of str.replace(/\s+/g, "")) { answer[letter] ? answer[letter] +=1: answer[letter] =1}
+
+  return answer
+}
+// console.log(countLetters("lighthouse in the house"))
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
